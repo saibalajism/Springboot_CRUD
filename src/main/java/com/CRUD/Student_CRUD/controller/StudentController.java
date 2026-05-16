7f091753d29bc5bf6397f9ef160c743377a3f9d8
Saibalaji SM <smsaibalaji@gmail.com>
2026-05-16 11:51:31 +0530

    update operation

--- src/main/java/com/CRUD/Student_CRUD/controller/StudentController.java
+++ src/main/java/com/CRUD/Student_CRUD/controller/StudentController.java
@@ -1,12 +1,16 @@
 package com.CRUD.Student_CRUD.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.CRUD.Student_CRUD.dto.StudentDto;
 import com.CRUD.Student_CRUD.entity.Student;
 import com.CRUD.Student_CRUD.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
+
 @RequestMapping("/student")
 public class StudentController {
 	
@@ -34,5 +41,10 @@ public class StudentController {
 	public Student fetchStudent(@PathVariable("StudentID") int StudentID) {
 		return studentService.fetchStudentbyId(StudentID);
 	}
-
+	@PutMapping("/update/{StudentID}")
+	public  ResponseEntity<Object> updateStudent(@RequestBody StudentDto dto, @PathVariable("StudentID") int StudentID) {
+		return studentService.updateStudent(dto, StudentID);
+	}
+	
 }
+
--- src/main/java/com/CRUD/Student_CRUD/service/StudentService.java
+++ src/main/java/com/CRUD/Student_CRUD/service/StudentService.java
@@ -3,16 +3,24 @@ package com.CRUD.Student_CRUD.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.CRUD.Student_CRUD.dto.StudentDto;
 import com.CRUD.Student_CRUD.entity.Student;
 import com.CRUD.Student_CRUD.repository.StudentRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class StudentService {
 	@Autowired
 	private StudentRepo studentRepository;
+	
+	
+	@Autowired
+	private ObjectMapper mapper;
 	
 	public Student StudentCreate(StudentDto dto)
 	{
@@ -29,6 +37,25 @@ public class StudentService {
 	}
 	public Student fetchStudentbyId(int StudentID) {
 		return studentRepository.findById(StudentID).get();		
+	}
+	public  ResponseEntity<Object>  updateStudent(StudentDto dto, int StudentId) {
+		Student a=studentRepository.findById(StudentId).orElse(null);
+		if(a==null) {
+			ObjectNode errorResponse = mapper.createObjectNode();
+            errorResponse.put("message", "Student does not exist");
+            errorResponse.put("httpStatusCode", 404);
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+		}else {
+			a.setStudentName(dto.getStudentName());
+			a.setMarks(dto.getMarks());
+		    a.setAddress(dto.getAddress());
+		    ObjectNode successResponse = mapper.createObjectNode();
+		    successResponse.put("message", "Student updated");
+		    successResponse.put("httpStatusCode", 200);
+		    successResponse.set("data",mapper.valueToTree(studentRepository.save(a)));
+		    return new ResponseEntity<>(successResponse, HttpStatus.OK);
+		}
+		
 	}
 
 }
